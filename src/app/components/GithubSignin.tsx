b25b78c53c0ca24d72319118d82460b4feabad40
Jeremy Vaartjes <jeremyv@hey.com>
2024-03-26 17:39:21 +1100

    Better way to stitch string together

--- src/app/components/GithubSignin.tsx
+++ src/app/components/GithubSignin.tsx
@@ -9,7 +9,7 @@ async function signInWithGithub() {
     const { error } = await supabase.auth.signInWithOAuth({
         provider: "github",
         options: {
-            redirectTo: window.location.origin + "/auth/callback",
+            redirectTo: `${window.location.origin}/auth/callback`,
         },
     });
 
